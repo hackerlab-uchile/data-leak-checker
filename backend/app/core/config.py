00b85a557caa57b28ebfc9befb1c1c43c85ac466
Nicolás Santibáñez <nsantibanezb@gmail.com>
2024-03-12 18:03:38 -0300

    wip: Working on new front page

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,4 +3,4 @@ import os
 POSTGRES_USER: str | None = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD: str | None = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_SERVER: str | None = os.environ.get("POSTGRES_SERVER")
-POSTGRES_DB: str | None = os.environ.get("POSTGRES_DATABASE")
+POSTGRES_DB: str | None = os.environ.get("POSTGRES_DB")
