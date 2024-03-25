7cbf1dbfe06ceba25b1398a5094716f407813bec
Nicolás Santibáñez <nsantibanezb@gmail.com>
2024-03-25 15:27:22 -0300

    feat: init_db creates databases if they don't exist

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -5,7 +5,12 @@ from models.data_leaks import DataLeak
 from models.data_types import DataType
 from models.emails import Email
 from models.passwords import Password
+from sqlalchemy_utils import create_database, database_exists
 
-# Inits database tables
+if not database_exists(engine.url):
+    create_database(engine.url)
 Base.metadata.create_all(bind=engine)
+
+if not database_exists(test_engine.url):
+    create_database(test_engine.url)
 Base.metadata.create_all(bind=test_engine)
