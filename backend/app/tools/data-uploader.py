524c1fe65427af735299915eb81f8dd59244b432
Nicolás Santibáñez <nsantibanezb@gmail.com>
2024-04-11 19:08:53 -0400

    wip: Experimenting with different ways to same emails by bulk insertions

--- backend/app/tools/data-uploader.py
+++ backend/app/tools/data-uploader.py
@@ -14,7 +14,7 @@ from models.rut_leaks import RutLeak
 from models.ruts import Rut
 from repositories.breach_repository import create_breach
 from repositories.data_type_repository import get_data_type_by_name, save_breach_data
-from repositories.email_repository import get_or_create
+from repositories.email_repository import create_all_emails, get_or_create
 from repositories.password_repository import add_or_create_all_passwords
 from schemas.breach_data import BreachDataCreate
 from schemas.breaches import BreachCreate
@@ -63,9 +63,10 @@ def handle_csv_file(f_path: str):
     for row in all_rows:
         values = row.split(sep)
         for i, c in enumerate(columns):
-            value = values[i]
-            if c == "password":
-                value = sha256(values[i].encode("UTF-8")).hexdigest()
+            value = sha256(values[i].encode("UTF-8")).hexdigest()
+            # value = values[i]
+            # if c == "password":
+            #     value = sha256(values[i].encode("UTF-8")).hexdigest()
             data[c].append(value)
     display_data(data)
     return data
@@ -145,6 +146,8 @@ def main() -> int:
         # 1. Guardamos el Breach
         created_breach = create_breach(db=session, breach=breach_to_create)
         for col, items in data_leak.items():
+            if col != "email":
+                continue
             # 2. Guardamos la info encontrada con el breach (nombre de columnas)
             column_data_type = get_data_type_by_name(db=session, name=col)
             if column_data_type is None:
@@ -161,7 +164,9 @@ def main() -> int:
             leak_data_name = "email_id"
             match column_data_type.dtype:
                 case "email":
-                    pass
+                    created_emails = create_all_emails(session, email_list=set(items))
+                    print("Se crearon un total de", len(created_emails), "correos.")
+                    continue
                 case "phone":
                     model = Phone
                     model_leak = PhoneLeak
@@ -171,35 +176,41 @@ def main() -> int:
                     model_leak = RutLeak
                     leak_data_name = "rut_id"
                 case "password":
-                    add_or_create_all_passwords(session, list_passwords=items)
+                    passwords_created = add_or_create_all_passwords(
+                        session, list_passwords=items
+                    )
+                    print(
+                        "Se crearon un total de", len(passwords_created), "contraseñas."
+                    )
                     continue
                 case _:
                     continue
-            for i, item in enumerate(items):
-                if item == "":  # or no tiene forma de Email
-                    continue
-                # a. Check if already exists
-                # a2. If doesnt exist -> save it
-                # a3. else -> get it
-                data = get_or_create(session, model, value=item)
-                for other_data in data_leak.keys():
-                    if other_data == col:
-                        continue
-                    column_data_type = get_data_type_by_name(
-                        db=session, name=other_data
-                    )
-                    if column_data_type is None:
-                        print(
-                            f"Error: Column with name {other_data} is not a valid data type!"
-                        )
-                        continue
-                    data_leak_created = get_or_create(
-                        db=session,
-                        model=model_leak,
-                        breach_id=created_breach.id,
-                        data_type_id=column_data_type.id,
-                        **{leak_data_name: data.id},
-                    )
+
+            # for i, item in enumerate(items):
+            # if item == "":  # or no tiene forma de Email
+            #     continue
+            #     # a. Check if already exists
+            #     # a2. If doesnt exist -> save it
+            #     # a3. else -> get it
+            #     data = get_or_create(session, model, value=item)
+            #     for other_data in data_leak.keys():
+            #         if other_data == col:
+            #             continue
+            #         column_data_type = get_data_type_by_name(
+            #             db=session, name=other_data
+            #         )
+            #         if column_data_type is None:
+            #             print(
+            #                 f"Error: Column with name {other_data} is not a valid data type!"
+            #             )
+            #             continue
+            #         data_leak_created = get_or_create(
+            #             db=session,
+            #             model=model_leak,
+            #             breach_id=created_breach.id,
+            #             data_type_id=column_data_type.id,
+            #             **{leak_data_name: data.id},
+            #         )
         # session.commit()
 
         # TODO: Guardamos los nuevos datos en su tabla (ej. Email), (si es que no existen ya)
