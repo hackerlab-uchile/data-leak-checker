fe66bf21aafbbe54cb11c729e269f78788d1f17f
Nicolás Santibáñez <nsantibanezb@gmail.com>
2024-05-06 16:59:45 -0400

    Added new endpoint for demo

--- backend/app/api/route_breach.py
+++ backend/app/api/route_breach.py
@@ -12,6 +12,7 @@ from schemas.breaches import BreachCreate
 from schemas.data_leak import DataLeakBase, DataLeakInput, DataLeakShow
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.expression import func
 
 router = APIRouter()
 
@@ -48,6 +49,31 @@ def get_breaches_info(payload: DataLeakInput, db: Session = Depends(get_db)):
         .join(Breach, Breach.id == DataLeak.breach_id)
         .filter(DataLeak.hash_value == hash_value)
         .filter(DataLeak.data_type_id == dtype.id)
+        .order_by(desc(Breach.breach_date))
+    ).all()
+    return found_breaches
+
+
+@router.post("/data/demo/", response_model=List[DataLeakShow])
+def get_breaches_demo(payload: DataLeakInput, db: Session = Depends(get_db)):
+    """Returns information about data breaches related to an email"""
+    # hash_value = sha256(payload.value.encode("UTF-8")).hexdigest()
+    # dtype = db.query(DataType).filter(DataType.name == payload.dtype).first()
+    rand_dl = (
+        db.query(DataLeak)
+        .join(DataType)
+        .filter(DataType.name == payload.dtype)
+        .order_by(func.random())
+        .first()
+    )
+    if rand_dl is None or payload.dtype == "phone":
+        # Por fines de demostración, consultar por teléfonos no entregará filtración
+        return []
+    found_breaches = (
+        db.query(DataLeak)
+        .join(Breach, Breach.id == DataLeak.breach_id)
+        .filter(DataLeak.hash_value == rand_dl.hash_value)
+        .filter(DataLeak.data_type_id == rand_dl.data_type_id)
         .order_by(desc(Breach.breach_date))
     ).all()
     return found_breaches
--- frontend/app/src/api/api.ts
+++ frontend/app/src/api/api.ts
@@ -14,6 +14,26 @@ export async function getDataLeaksByValueAndType(
   let response;
   const data = { value: query, dtype: type };
   try {
+    response = await apiClient.post<DataLeak[]>("/breach/data/demo/", data);
+    return response.data;
+  } catch (error: any) {
+    if (error.response && error.response.status === 404) {
+      return [];
+    } else {
+      // TODO: Informar que ha ocurrido un error
+      return [];
+      // throw error;
+    }
+  }
+}
+
+export async function getDataLeaksByValueAndTypeReal(
+  query: string,
+  type: QueryType
+): Promise<DataLeak[]> {
+  let response;
+  const data = { value: query, dtype: type };
+  try {
     response = await apiClient.post<DataLeak[]>("/breach/data/", data);
     return response.data;
   } catch (error: any) {
